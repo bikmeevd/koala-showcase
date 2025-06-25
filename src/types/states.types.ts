386149121020dc3163f04a34468905dff75a8728
Danis Bikmeev <bikmeevd00@gmail.com>
2025-06-25 18:17:46 +0200

    feat: add state types

---
+++ src/types/states.types.ts
@@ -0,0 +1,13 @@
+import type { HierarchyItem } from "./hierarchy";
+
+// Stav celé aplikace - co si pamatuje
+export interface HierarchyState {
+  items: HierarchyItem[];
+  expandedItems: Set<string>;
+}
+
+// Akce které můžu dělat - klikat a mazat
+export interface HierarchyActions {
+  toggleExpand: (path: number[], relationshipType?: string) => void;
+  deleteItem: (itemId: string, path: number[]) => void;
+}
